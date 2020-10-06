5475e48bdc80e927a031129c2576cc9babd043ac
srisoumya02 <69790885+srisoumya02@users.noreply.github.com>
2020-10-06 13:06:26 +0530

    Add files via upload

---
+++ Coursera_Capstone_Github.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Coursera_Capstone_Github
+
+# In[1]:
+
+
+#importing pandaslibrary
+import pandas as pd
+#importing numpy library
+import numpy as np
+
+
+# In[2]:
+
+
+#print new line
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
